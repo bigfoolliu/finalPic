1fbde0d54e7f4b0d2629f032a48be30a1a31c1dc
bigfoolliu <liu15671677014@icloud.com>
2025-07-25 13:50:57 +0800

    Initial Commit

---
+++ finalPic/finalPicApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalPicApp.swift
+//  finalPic
+//
+//  Created by 刘瑞 on 2025/7/25.
+//
+
+import SwiftUI
+
+@main
+struct finalPicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ finalPicTests/finalPicTests.swift
@@ -0,0 +1,17 @@
+//
+//  finalPicTests.swift
+//  finalPicTests
+//
+//  Created by 刘瑞 on 2025/7/25.
+//
+
+import Testing
+@testable import finalPic
+
+struct finalPicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
